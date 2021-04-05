5c23e7fe55448a55a02306f259089568ddb20b87
Yevette Dorene <ydorene@pl-j1.msk.21-school.ru>
2021-04-05 17:19:15 +0300

    2

--- get_next_line.h
+++ get_next_line.h
@@ -1,21 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ydorene <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/31 22:30:59 by ydorene           #+#    #+#             */
-/*   Updated: 2021/01/23 21:25:31 by ydorene          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdio.h>
+# define BUFFER_SIZE 65
 
 int				ret(int readed, char *ostatok);
 int				get_next_line(int fd, char **line);
